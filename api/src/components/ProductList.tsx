cdb8268a177ab304bc2b2a7ae78b507fd6a02a88
marius_tataru <marius.tataru.02@gmail.com>
2024-02-21 15:56:48 +0200

    fetching

--- api/src/components/ProductList.tsx
+++ api/src/components/ProductList.tsx
@@ -1,25 +1,71 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+export const URL = 'https://dummyjson.com/products'
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+
 
 export const ProductList = () => {
 
- const [productArray, setProductArray] = useState([]);
+ const [products, setProducts] = useState([]);
 
- useEffect(() => {
-
-  async function getProductList() {
-
-    const response = await fetch("https://dummyjson.com/products");
-    const products = await response.json();
-    setProductArray(products.products)
-
- }
- getProductList();
- } , [])
+  useEffect(() => {
+  axios.get(URL).then((res) => setProducts(res.data.products))}
+ )
+  console.log(products)
 
   return (
-    <div>
-        <p>{products}</p>
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell align="right">Title</TableCell>
+            <TableCell align="right">Description</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {products.map((product) => (
+            <TableRow
+              key={product.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {product.id}
+              </TableCell>
+              <TableCell align="right">{product.title}</TableCell>
+              <TableCell align="right">{product.description}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
 
 
   )
